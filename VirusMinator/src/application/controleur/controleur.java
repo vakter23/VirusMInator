cc82ae0faea565d99445d009f2870878ccee9894
Maxine Lama <maxineLm@users.noreply.github.com>
2020-05-06 17:25:33 +0200

    tab2D

--- VirusMinator/src/application/controleur/controleur.java
+++ VirusMinator/src/application/controleur/controleur.java
@@ -23,6 +23,10 @@ public class controleur {
 
 		List<Integer> listeMap  = Arrays.asList(BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BlocVertSpawnMob, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, 206, 207, Sable, Sable, BordTerrain, BlocVertSpawnMob, BlocVertSpawnMob, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, BlocVertSpawnMob, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BlocSpawnMob, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Sable, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, BlocVertSpawnMob, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BlocVertSpawnMob, BlocVertSpawnMob, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BlocVertSpawnMob, BlocVioletSpawnMobSurement, BlocVioletSpawnMobSurement, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BordTerrain, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BlocBlancHopital, BlocBlancHopital, BlocBlancHopital, Sable, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BlocBlancHopital, 445, BlocBlancHopital, 445, BlocBlancHopital, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Chemin, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BlocBlancHopital, 445, BlocBlancHopital, 445, BlocBlancHopital, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, Chemin, BlocBlancHopital, 445, 445, 445, BlocBlancHopital, BLocBasDroiteArriveMob, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, Sable, BlocBlancHopital, 445, BlocBlancHopital, 445, BlocBlancHopital, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BlocBlancHopital, 445, BlocBlancHopital, 445, BlocBlancHopital, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BlocBlancHopital, BlocBlancHopital, BlocBlancHopital, Herbe, Chemin, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, Herbe, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain, BordTerrain);
 		String tabMap[][] = new String[32][32];
+		
+		
+			
+	
 
 		
 		for (int i = 0; i <tabMap.length; i++) {
@@ -32,57 +36,57 @@ public class controleur {
 				
 			
 			
-				if(listeMap.get(i)==BordTerrain) {
+				if(listeMap.get(i*j)==BordTerrain) {
 					tabMap[i][j]= "BordTerrain";
 				}
 					
-				else if(listeMap.get(i)==Herbe) {
+				else if(listeMap.get(i*j)==Herbe) {
 					tabMap[i][j]= "Herbe";
 
 				}
 				
-				else if(listeMap.get(i)==Sable) {
+				else if(listeMap.get(i*j)==Sable) {
 					tabMap[i][j]= "Sable";
 
 				}
 				
-				else if(listeMap.get(i)==BlocVioletSpawnMobSurement) {
+				else if(listeMap.get(i*j)==BlocVioletSpawnMobSurement) {
 					tabMap[i][j]= "BlocVioletSpawnMobSurement";
 
 				}
 				
 				
-				else if(listeMap.get(i)==BlocVertSpawnMob) {
+				else if(listeMap.get(i*j)==BlocVertSpawnMob) {
 					tabMap[i][j]= "BlocVertSpawnMob";
 
 				}
 				
-				else if(listeMap.get(i)==Chemin) {
+				else if(listeMap.get(i*j)==Chemin) {
 					tabMap[i][j]= "Chemin";
 
 				}
 				
-				else if(listeMap.get(i)==BlocBlancHopital) {
+				else if(listeMap.get(i*j)==BlocBlancHopital) {
 					tabMap[i][j]= "BlocBlancHopital";
 
 				}
 				
-				else if(listeMap.get(i)==PlacementTourelles) {
+				else if(listeMap.get(i*j)==PlacementTourelles) {
 					tabMap[i][j]= "PlacementTourelles";
 
 				}
 				
-				else if(listeMap.get(i)==BlocRougeHopital) {
+				else if(listeMap.get(i*j)==BlocRougeHopital) {
 					tabMap[i][j]= "BlocRougeHopital";
 
 				}
 				
-				else if(listeMap.get(i)==BLocBasDroiteArriveMob) {
+				else if(listeMap.get(i*j)==BLocBasDroiteArriveMob) {
 					tabMap[i][j]= "BLocBasDroiteArriveMob";
 
 				}
 				
-				else if(listeMap.get(i)==BlocSpawnMob) {
+				else if(listeMap.get(i*j)==BlocSpawnMob) {
 					tabMap[i][j]= "BlocSpawnMob";
 
 				}
