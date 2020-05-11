a1e8edc7d2f5c7475154a1ade2a81a668e073e82
akter <akter@192.168.1.25>
2020-05-11 12:20:51 +0200

    Clean

--- JeuProject/src/application/Test.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class Test {
-
-	public static void main(String[]args) {
-		System.out.println("WELCOME TO THE PARTY");
-		System.out.println("J'espere que je maitrise mon espace");
-	}
-}
