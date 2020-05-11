512f9670b0249450e1544b7990d3c0a40ec870df
Djibril QUENUM-SANFO <d.quenumsanfo@gmail.com>
2020-05-11 16:45:07 +0200

    Affichage de la map ;)

--- VirusMinator/src/application/Main.java
+++ VirusMinator/src/application/Main.java
@@ -2,22 +2,33 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        BorderPane root = FXMLLoader.load(getClass().getResource("application.vue/tilemap.fxml"));
-        Scene scene = new Scene(root,900,800);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            URL url = new File("src/application/vue/tilemap.fxml").toURI().toURL();
+            loader.setLocation(url);
+            BorderPane root = new BorderPane();
+            root = loader.load();
+            Scene scene = new Scene(root, 900, 900);
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("VirusMinator");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            root.requestFocus();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 
     public static void main(String[] args) {
         launch(args);
